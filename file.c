cc6f6d8a37d587db5193f94d1c47a68b092106b4
AshwiniK10 <35912753+AshwiniK10@users.noreply.github.com>
2020-01-10 09:23:02 +0530

    Update file.c

--- file.c
+++ file.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 int main()
 {
-    int n=10;
+    int n=10, i;
     unsigned long long factorial = 1;
     printf("Enter an integer: ");
     if (n < 0)
         printf("Error! Factorial of a negative number doesn't exist.");
     else
     {
-        for(int i=1; i<=n; ++i)
+        for(i=1; i<=n; ++i)
         {
             factorial *= i;            
         }
