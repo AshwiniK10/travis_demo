4ad1a2b2889c5cb039b0bf356b288bb02150894b
AshwiniK10 <35912753+AshwiniK10@users.noreply.github.com>
2020-03-06 09:02:06 +0530

    Update test.c

--- test.c
+++ test.c
@@ -5,7 +5,7 @@ from file import main
 class MyTest(unittest.TestCase): 
     def test_my_function(self):
         self.assertEqual(main('-1'), 0),
-  self.assertEqual(main('2'), 0),
+  self.assertEqual(main('2'), factorial),
         
         
 if __name__ == '__main__':
