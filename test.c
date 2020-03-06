4bfefa87a40be30e9869130ff8dcb4b158bdbfc8
AshwiniK10 <35912753+AshwiniK10@users.noreply.github.com>
2020-03-06 08:53:54 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,1 +1,11 @@
+import unittest 
+from file import main
+# import main
 
+class MyTest(unittest.TestCase): 
+    def test_my_function(self):
+        self.assertEqual(palindromCheck('abc'), 0)
+        
+        
+if __name__ == '__main__':
+    unittest.main()
